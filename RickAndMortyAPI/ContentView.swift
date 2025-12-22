7de3f13ea31955637fe0d364ec1a16d0635f2094
josetomd <isset.josset@gmail.com>
2025-12-22 12:04:58 -0300

    Initial Commit

---
+++ RickAndMortyAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyAPI
+//
+//  Created by Josset Garcia on 22-12-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyAPI/RickAndMortyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAPIApp.swift
+//  RickAndMortyAPI
+//
+//  Created by Josset Garcia on 22-12-25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
