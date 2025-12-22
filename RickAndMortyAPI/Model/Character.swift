3a7e79b9fc17583a2ab8f5b09891b744d9ad3df6
josetomd <isset.josset@gmail.com>
2025-12-22 15:07:15 -0300

    add character model

---
+++ RickAndMortyAPI/Model/Character.swift
@@ -0,0 +1,63 @@
+//
+//  Character.swift
+//  RickAndMortyAPI
+//
+//  Created by Josset Garcia on 22-12-25.
+//
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let characterInfo = try? JSONDecoder().decode(CharacterInfo.self, from: jsonData)
+
+import Foundation
+
+// MARK: - CharacterInfo
+struct CharacterResult: Codable {
+    let info: Info
+    let results: [Character]
+}
+
+// MARK: - Info
+struct Info: Codable {
+    let count, pages: Int
+    let next: String
+    let prev: String?
+}
+
+// MARK: - Result
+struct Character: Codable {
+    let id: Int
+    let name: String
+    let status: Status
+    let species: Species
+    let type: String
+    let gender: Gender
+    let origin, location: Location
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+enum Gender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case unknown = "unknown"
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let name: String
+    let url: String
+}
+
+enum Species: String, Codable {
+    case alien = "Alien"
+    case human = "Human"
+}
+
+enum Status: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
