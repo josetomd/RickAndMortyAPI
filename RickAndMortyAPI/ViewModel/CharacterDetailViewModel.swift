7f922b06e8ea74475239f98398f0460a73f40297
josetomd <isset.josset@gmail.com>
2025-12-26 10:01:20 -0300

    refactor to parallel fetch of episodes

--- RickAndMortyAPI/ViewModel/CharacterDetailViewModel.swift
+++ RickAndMortyAPI/ViewModel/CharacterDetailViewModel.swift
@@ -19,17 +19,32 @@ class CharacterDetailViewModel: ObservableObject {
         self.service = service
     }
     
+    @MainActor
     func fetchEpisodes() async {
         self.isLoading = true
-        for episode in character.episode {
-            do {
-                if let ep = try await service.fetchEpisode(url: episode) {
-                    self.episodes.append(ep)
-                }
-            } catch {
-                self.errorMessage = error.localizedDescription
-            }
+        defer {
+            self.isLoading = false
         }
-        self.isLoading = false
+        
+        do {
+            let fetchedEpisodes = try await withThrowingTaskGroup(of: Episode?.self) { group in
+                for urlString in character.episode {
+                    group.addTask {
+                        try await self.service.fetchEpisode(url: urlString)
+                    }
+                }
+                
+                var results: [Episode] = []
+                for try await episode in group {
+                    if let episode = episode {
+                        results.append(episode)
+                    }
+                }
+                return results
+            }
+            self.episodes = fetchedEpisodes.sorted(by: { $0.id < $1.id })
+        } catch {
+            self.errorMessage = error.localizedDescription
+        }
     }
 }
